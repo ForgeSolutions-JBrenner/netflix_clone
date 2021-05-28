764fc136b17ffb8b04386f832c3e334bf9ec1c85
ForgeSolutions-JBrenner <justin.brenner@forgetechnologysolutions.com>
2021-05-28 16:37:26 -0400

    added a bunch of stuff

---
+++ src/Banner.js
@@ -0,0 +1,57 @@
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import requests from "./requests";
+import "./Banner.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTopRated);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1) //-1 so that we do not risk going over specific length of string this will get one of the netflix originals randomly
+        ]
+      );
+	  return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(str, n) {
+	  return str?.length > n ? str.substr(0, n-1) + "..." : str;
+  }
+  return (
+    <header className="banner"
+		style={{
+			backgroundSize: "cover",
+			backgroundImage: `url(
+				"${base_url}${movie?.backdrop_path}"
+			)`,
+			backgroundPosition: "center center",
+		}}
+		>
+		<div className="banner_contents">
+	  <h1 className="banner_title">
+		  {movie?.title || movie?.name || movie?.original_name}
+	  </h1>
+
+	  <div className="banner_buttons">
+		  <button className="banner_button">Play</button>
+		  <div className="divider"/>
+		  <button className="banner_button">My List</button>
+		  <h1 className="banner_description">
+			  {truncate(movie?.overview, 150)}
+		  </h1>
+	  </div>
+	  </div>
+    </header>
+  );
+}
+
+export default Banner;
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({title}) {
-	const [movies, setMovies] = useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-	return (
-		<div>
-			<h2>{title}</h2>
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  //run code based on specific condition or variable
+  useEffect(() => {
+    //if [] blank then run once when the row load and dont run again
+    //when anything use in useeffect with a variable being pulled from outside and being used inside of useeffect, you have to use that variable in the use effect,
+    async function fetchData() {
+      const request = await axios.get(fetchUrl); //await is saying when the request is made it will wait for the promise to come back and then do something
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-		</div>
-	)
+  //console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {/*row posters go here */}
+        {movies.map((movie) => (
+          <img
+		  key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ?  movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
+//a deafult is actually renamable you can have one standard export default but can export const many times from single file
